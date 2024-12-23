fbb70548b1d360aa69760250ac1b1fd252d05965
Andrew Brown <andrew@exampro.co>
2024-12-23 14:06:17 -0500

    ytty

---
+++ gemini-code-assist/linktree-clone/links.sql
@@ -0,0 +1,5 @@
+INSERT INTO links (url, title) VALUES ('https://www.example.com', 'Example Website');
+INSERT INTO links (url, title) VALUES ('https://www.google.com', 'Google Search');
+INSERT INTO links (url, title) VALUES ('https://www.youtube.com', 'YouTube');
+INSERT INTO links (url, title) VALUES ('https://github.com', 'GitHub');
+INSERT INTO links (url, title) VALUES ('https://www.wikipedia.org', 'Wikipedia');
